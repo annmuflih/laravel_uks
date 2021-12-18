0063563a018ee0ca373823f7835c30177b4caf12
rodhisz <rodhisz.45@gmail.com>
2021-12-18 22:15:51 +0700

    fix chart controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -65,7 +65,7 @@ class HomeController extends Controller
 
         $months = Pasien::select(
             //PostgreSQL
-            DB::raw("to_char(created_at, '%m') as month")
+            DB::raw("to_char(created_at, 'mm') as month")
             //MySQL
             //DB::raw("Month(created_at) as month")
             )
