d97395a4c328c6b07ad9cdcf2f3da72ab568dd4a
rodhisz <rodhisz.45@gmail.com>
2021-12-18 10:12:54 +0700

    fix chart controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -50,12 +50,12 @@ class HomeController extends Controller
         //chart
         $pasiens = Pasien::select(DB::raw("COUNT(*) as count"))
                 ->whereYear('created_at', date('Y'))
-                ->groupBy(DB::raw("Month(created_at)"))
+                ->groupBy(DB::raw("month(created_at)"))
                 ->pluck('count');
 
         $months = Pasien::select(DB::raw("Month(created_at) as month"))
                 ->whereYear('created_at', date('Y'))
-                ->groupBy(DB::raw("Month(created_at)"))
+                ->groupBy(DB::raw("month(created_at)"))
                 ->pluck('month');
 
         $datas = [0,0,0,0,0,0,0,0,0,0,0,0];
