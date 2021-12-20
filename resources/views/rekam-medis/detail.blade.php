ba45787da5057e38d5edc8ae795ca44f5a765c90
rodhisz <rodhisz.45@gmail.com>
2021-12-20 16:54:51 +0700

    fix detail rekam medis blade

--- resources/views/rekam-medis/detail.blade.php
+++ resources/views/rekam-medis/detail.blade.php
@@ -20,41 +20,59 @@ Detail Riwayat Penyakit
                     @else
                         Detail Riwayat Penyakit Pasien <b style="color: black">{{$riwayat_penyakit->pasien->nama_pasien}}</b>
                     @endempty
-
                 </div>
                 <div class="card-body">
-                    <table class="table table-hover">
-                        <thead>
-                            <tr class="text-center">
-                                <th>ID Riwayat Sakit</th>
-                                <th>Keluhan</th>
-                                <th>Tindakan</th>
-                                <th>Status Pasien</th>
-                                <th>Nama Petugas</th>
-                                <th>Tanggal</th>
-                                <th>Jam</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($detail as $row)
-                            <tr class="text-center">
-                                <td>{{$row->id}}</td>
-                                <td>{{$row->keluhan}}</td>
-                                <td>{{$row->tindakan}}</td>
-                                <td>
-                                    @if ($row->status_pasien == 'Rawat')
-                                        <span class="badge bg-warning" style="color: white">Rawat</span>
-                                    @else
-                                        <span class="badge bg-primary" style="color: white">Rawat Jalan</span>
-                                    @endif
-                                </td>
-                                <td>{{$row->petugas->nama_petugas}}</td>
-                                <td>{{$row->created_at->format('d/m/Y')}}</td>
-                                <td>{{$row->created_at->format('H:m:s')}}</td>
-                            </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
+                    @empty($detail)
+                        <table class="table table-hover">
+                            <thead>
+                                <tr class="text-center">
+                                    <th>ID Riwayat Sakit</th>
+                                    <th>Keluhan</th>
+                                    <th>Tindakan</th>
+                                    <th>Status Pasien</th>
+                                    <th>Nama Petugas</th>
+                                    <th>Tanggal</th>
+                                    <th>Jam</th>
+                                </tr>
+                            </thead>
+                        </table>
+                        <div class="text-center mt-3">
+                            <span>Rekam Medis Belum Tersedia</span>
+                        </div>
+                    @else
+                        <table class="table table-hover">
+                            <thead>
+                                <tr class="text-center">
+                                    <th>ID Riwayat Sakit</th>
+                                    <th>Keluhan</th>
+                                    <th>Tindakan</th>
+                                    <th>Status Pasien</th>
+                                    <th>Nama Petugas</th>
+                                    <th>Tanggal</th>
+                                    <th>Jam</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($detail as $row)
+                                <tr class="text-center">
+                                    <td>{{$row->id}}</td>
+                                    <td>{{$row->keluhan}}</td>
+                                    <td>{{$row->tindakan}}</td>
+                                    <td>
+                                        @if ($row->status_pasien == 'Rawat')
+                                            <span class="badge bg-warning" style="color: white">Rawat</span>
+                                        @else
+                                            <span class="badge bg-primary" style="color: white">Rawat Jalan</span>
+                                        @endif
+                                    </td>
+                                    <td>{{$row->petugas->nama_petugas}}</td>
+                                    <td>{{$row->created_at->format('d/m/Y')}}</td>
+                                    <td>{{$row->created_at->format('H:m:s')}}</td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    @endempty
                 </div>
             </div>
         </div>
