e276db32cd5743d905e204b54d7aebd6360e0626
rodhisz <rodhisz.45@gmail.com>
2022-10-22 22:18:31 +0700

    fix seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +15,23 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('bismillahsecret'),
+            'role' => 'admin',
+        ]);
+        User::create([
+            'name' => 'Petugas UKS',
+            'email' => 'petugas@gmail.com',
+            'password' => Hash::make('secret12435'),
+            'role' => 'petugas',
+        ]);
+        User::create([
+            'name' => 'Visitor',
+            'email' => 'visitor@gmail.com',
+            'password' => Hash::make('1234'),
+            'role' => 'visitor',
+        ]);
     }
 }
